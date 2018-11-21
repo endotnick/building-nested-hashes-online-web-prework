be27dc9db16c8e3684bd8a6b609b02a10cde3a12
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-21 05:37:59 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,20 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+        #your hashes here!
+      ]
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+        #your hashes here!
+      ]
       }}
 
   
